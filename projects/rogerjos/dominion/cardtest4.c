1de9fae431b57cb63b150625a91ffb84e2a329bc
Joshua Rogers <rogerjos@oregonstate.edu>
2018-02-04 12:12:30 -0800

    Ready to test all tests. Into the breach\!

--- projects/rogerjos/dominion/cardtest4.c
+++ projects/rogerjos/dominion/cardtest4.c
@@ -17,53 +17,67 @@
 
 #define SEED 23
 
-//TODO: All of it. :-/
-
-
 int main() {
 	struct gameState state,	// Current game state 
 					 control;	// For detecting and reverting state changes
 
 	int	player,
-		crashErrors = 0,	// Counter for smithy returning errors
-		handCountErrors = 0,	// Counter for test failures
-		deckCountErrors = 0,	// Counter for test failures
-		playedCountErrors = 0,	// Counter for test failures
-		playedErrors = 0,	// Counter for test failures
-		totalErrors = 0;	// Counter for cumulative errors
+		treasureCount,	
+		crashErrors = 0,	
+		handCountErrors = 0,	
+		treasureErrors = 0;
+		deckCountErrors = 0,	
+		playedCountErrors = 0,	
+		playedErrors = 0,
+		totalErrors = 0;
 
-	int k[10] = {adventurer, council_room, feast, gardens, mine,	// Arbitrary cards
+	int k[10] = {cutpurse, council_room, feast, gardens, mine,	// Arbitrary cards
 					remodel, smithy, village, baron, great_hall};
+	
+	int treasure[2] = {-1, -1};	 // Holds last two cards drawn for treasure check
 
 	/* Set up game */
 	memset(&state, 23, sizeof(struct gameState)); // Clear data
 	if (initializeGame(MAX_PLAYERS, k, SEED, &state)) {	// Print error and exit if new game fails
-			printf("smithy ABORT: initializeGame() exited with error.\n");
+			printf("adventurer ABORT: initializeGame() exited with error.\n");
 			return 1;
 	}
 	memcpy(&control, &state, sizeof(struct gameState));	// Save beginning game state	
 	
 	for (player = 0; player < MAX_PLAYERS; player++) {	// Run test for each player
 
-		/* Get and play smithy */
+		/* Get and play adventurer */
 		drawCard(player, state);	// Draw a card
-		state.hand[player][state.handCount[player] - 1] = smithy // Set most recently drawn card to smithy
-		crashErrors += playCard(state.handCount[player] - 1, 0, 0, 0, &state);	// Play smithy and check for error
+		state.hand[player][state.handCount[player] - 1] = adventurer // Set most recently drawn card to adventurer
+		crashErrors += playCard(state.handCount[player] - 1, 0, 0, 0, &state);	// Play adventurer and check for error
 
 		/* Test outcome */
-		if (state.handCount[player] != control.handCount[player] + 2) { // Smithy draws 3 when played, so +2 net to hand
+		if (state.handCount[player] != control.handCount[player] + 1) { // Adventurer draws 2 when played, so +1 net to hand
 			handCountErrors++;
 		}
 
-		if ((state.deckCount[player] + state.discardCount[player]) != (control.deckCount[player] + control.discardCount[player] - 3)) {	// Smithy draws 3, so three cards should be missing between deck and discard (discard shuffled if < 3 in deck)
+		if (state.handCount[player] < 2) { // Adventurer draws two treasures, so hand must contain at least two cards
+			treasureErrors++;
+		}
+		else {	// Check whether most recently-drawn two cards are treasures
+			treasure[0] = state.hand[player][state.handCount[player] - 1];	// treasure[0] = most recently drawn card
+			treasure[1] = state.hand[player][state.handCount[player] - 2];	// trasure [0] = next most recently drawn card
+			if ((treasure[0] != copper && (treasure[0] != silver && treasure[0] != gold))
+				||
+				(treasure[1] != copper && (treasure[1] != silver && treasure[1] != gold))) {
+				treasureErrors++;
+			}
+		}
+
+		if ((state.deckCount[player] + state.discardCount[player]) != (control.deckCount[player] + control.discardCount[player] - 2)) {	// Adventurer draws 2, and puts non-treasures into discard, so two cards should be missing between deck and discard
 			deckCountErrors++;
 		}
 
-		if (state.playedCardCount != control.playedCardCount + 1) {	// Smithy was played, so playedCardCount should increment
+		if (state.playedCardCount != control.playedCardCount + 1) {	// Adventurer was played, so playedCardCount should increment
 			playedCountErrors++;
 		}
 
-		if (!((state.playedCardCount > 0) && (state.playedCards[state.playedCardCount - 1] == smithy))) {	// playedCardCount incremented and top card of playedCards should be smithy
+		if (!((state.playedCardCount > 0) && (state.playedCards[state.playedCardCount - 1] == adventurer))) {	// playedCardCount incremented and top card of playedCards should be adventurer
 			playedErrors++;
 		}
 
@@ -77,7 +91,6 @@ int main() {
 		memcpy(&state.playedCardCount, &control.playedCardCount, sizeof(int));	// playedCardCount
 		memcpy(state.playedCards, control.playedCards, sizeof(int) * MAX_DECK);	// playedCards
 
-
 		if (memcmp(control, state, sizeof(struct gameState))) {	// Check for out of bound changes to game state 	
 			errors++;
 		}	
@@ -85,7 +98,7 @@ int main() {
 	}
 		
 	/* Display results crash */
-	printf("smithy: ");
+	printf("adventurer: ");
 	if (crashErrors) {
 		printf("FAIL does not return error\n");
 	} 
@@ -93,35 +106,44 @@ int main() {
 		printf("PASS does not return error\n");
 	}
 
-	/* Display results draw 3 cards */
-	printf("smithy: ");
+	/* Display results draw 2 cards */
+	printf("adventurer: ");
 	if (handCountErrors) {
-		printf("FAIL draws three cards into correct hand\n");
+		printf("FAIL draws two cards into correct hand\n");
 	} 
 	else {
-		printf("PASS draws three cards into correct hand\n");
+		printf("PASS draws two cards into correct hand\n");
 	}
 
-	/* Display results deck+discard size reduced by three */
-	printf("smithy: ");
-	if (deckCountErrors) {
-		printf("FAIL draws three cards from correct deck\n");
+	/* Display results cards drawn both treasures */
+	printf("adventurer: ");
+	if (treasureErrors) {
+		printf("FAIL cards drawn both treasures\n");
 	} 
 	else {
-		printf("PASS draws three cards from correct deck\n");
+		printf("PASS cards drawn both treasures\n");
+	}
+
+	/* Display results deck+discard size reduced by two */
+	printf("adventurer: ");
+	if (deckCountErrors) {
+		printf("FAIL two cards removed from correct deck + discard\n");
+	} 
+	else {
+		printf("PASS two cards removed from correct deck + discard\n");
 	}
 	
 	/* Display results played count incremented */
-	printf("smithy: ");
+	printf("adventurer: ");
 	if (playedCountErrors) {
 		printf("FAIL played card count incremented\n");
 	} 
 	else {
-		printf("PASS played card cound incremented\n");
+		printf("PASS played card count incremented\n");
 	}
 	
-	/* Display results smithy placed on top of played card pile */
-	printf("smithy: ");
+	/* Display results adventurer placed on top of played card pile */
+	printf("adventurer: ");
 	if (playedErrors) {
 		printf("FAIL placed on top of played card pile\n");
 	} 
@@ -129,9 +151,11 @@ int main() {
 		printf("PASS placed on top of played card pile\n");
 	}
 
+	totalErrors = crashErrors + handCountErrors + treasureErrors + deckCountErrors + playedCountErrors + playedErrors;
+
 	/* Display final results */
-	if (!(crashErrors || handCountErrors || deckCountErrors || playedCountErrors || playedErrors)){
-		printf("smithy: ALL TESTS PASSED");
+	if (!totalErrors) {
+		printf("adventurer: ALL TESTS PASSED");
 	}
 
 	return 0;
